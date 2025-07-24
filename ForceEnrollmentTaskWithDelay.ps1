94635ca14e5a95c572bd553c67b47f83aa12768c
j0eyv <41282854+j0eyv@users.noreply.github.com>
2025-07-24 20:49:02 +0200

    Add files via upload

---
+++ ForceEnrollmentTaskWithDelay.ps1
@@ -0,0 +1,10 @@
+#region ForceEnrollment script
+$action = New-ScheduledTaskAction -Execute "$env:windir\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File `"$($env:SystemDrive)\ForceEnrollmentV2.ps1`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger.Delay = 'PT3M'
+$principal = New-ScheduledTaskPrincipal 'NT Authority\SYSTEM' -RunLevel Highest
+$settingsSet = New-ScheduledTaskSettingsSet
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settingsSet
+Register-ScheduledTask -TaskName 'AVD-MDM-Enrollment' -InputObject $task -ErrorAction Ignore
+Enable-ScheduledTask -TaskName 'AVD-MDM-Enrollment'
+#endregion
